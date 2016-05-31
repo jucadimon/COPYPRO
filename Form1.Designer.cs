9f2543b0bfc17881f11841504e7638aa60bad099
Juan Carlos Diaz Montiel <jucadimon07@gmail.com>
2016-05-31 18:09:18 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace COPYPRO
+{
+    partial class FormularioOpaco
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtCaja = new System.Windows.Forms.TextBox();
+            this.btnExportarTXT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Location = new System.Drawing.Point(713, 12);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 36);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInfo.Location = new System.Drawing.Point(632, 12);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(75, 36);
+            this.btnInfo.TabIndex = 1;
+            this.btnInfo.Text = "Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(250, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(301, 47);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "C O P Y P R O 2016";
+            // 
+            // txtCaja
+            // 
+            this.txtCaja.AcceptsReturn = true;
+            this.txtCaja.AcceptsTab = true;
+            this.txtCaja.AllowDrop = true;
+            this.txtCaja.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.txtCaja.ForeColor = System.Drawing.Color.White;
+            this.txtCaja.Location = new System.Drawing.Point(51, 126);
+            this.txtCaja.Multiline = true;
+            this.txtCaja.Name = "txtCaja";
+            this.txtCaja.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCaja.Size = new System.Drawing.Size(700, 432);
+            this.txtCaja.TabIndex = 3;
+            this.txtCaja.Text = "Escriba aquí...";
+            // 
+            // btnExportarTXT
+            // 
+            this.btnExportarTXT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportarTXT.Location = new System.Drawing.Point(51, 84);
+            this.btnExportarTXT.Name = "btnExportarTXT";
+            this.btnExportarTXT.Size = new System.Drawing.Size(141, 36);
+            this.btnExportarTXT.TabIndex = 4;
+            this.btnExportarTXT.Text = "Exportar a txt...";
+            this.btnExportarTXT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExportarTXT.UseVisualStyleBackColor = true;
+            this.btnExportarTXT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormularioOpaco
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(45)))), ((int)(((byte)(168)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.btnExportarTXT);
+            this.Controls.Add(this.txtCaja);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnSalir);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(600, 480);
+            this.Name = "FormularioOpaco";
+            this.Opacity = 0.6D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "COPYPRO Escribe sin limites v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtCaja;
+        private System.Windows.Forms.Button btnExportarTXT;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COPYPRO
+{
+    public partial class FormularioOpaco : Form
+    {
+        public string ostipo = "";
+        public bool SistemaOperativo;
+        public StringBuilder archivoGenerado;
+        //Creamos el escritor de archivos y damos la ruta, nombre del archivo y modo de creado
+        FileStream archivoGeneradoA;
+        string directorio1;
+
+        public FormularioOpaco()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//Codigo para boton salir
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            Button bsalirSub = new Button();
+            Label Titulo_SubFormulario = new Label();
+            Label Informacion = new Label();
+            Form Subformulario = new Form();
+            //  datos del boton salir
+            bsalirSub.Font = new Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
+            bsalirSub.Location = new Point(585, 10);
+            bsalirSub.Name = "bsalirSub";
+            bsalirSub.Size = new System.Drawing.Size(45, 25);
+            bsalirSub.Text = "X";
+            bsalirSub.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;//MiddleCenter/TopCenter
+            bsalirSub.FlatStyle = FlatStyle.Flat;
+            //  datos del label titulo
+            Titulo_SubFormulario.Name = "Titulo_SubFormulario";
+            Titulo_SubFormulario.Text = "I N F O R M A C I O N";
+            Titulo_SubFormulario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;//MiddleCenter/TopCenter
+            Titulo_SubFormulario.Size = new Size(400, 25);//Tamaño del label
+            Titulo_SubFormulario.Location = new Point(320 - (Titulo_SubFormulario.Width / 2), 10);//localizacion del label en el formulario
+            Titulo_SubFormulario.Font = new Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            //  datos del label Informacion
+            Informacion.Name = "Informacion";
+            Informacion.Text = "INSTRUCCIONES DE USO: "+
+                "\nLocalice la informacion que desea escribir desde un archivo, pagina web, "
+                +"u otro programa, a continuación abra el programa COPYPRO y escriba dentro de la caja de "
+                + "texto y al final copie y pegue lo escrito en su editor favorito."
+                + "\nAl presionar el boton ExportarTXT se crea un archivo de texto multiple con el historial "
+                + "del contenido de la caja de texto."
+                + "\n\nSOFTWARE DE ESCRITURA RAPIDA"
+            + "\n\nSOFTWARE CREADO POR:\nJUANCARLOS DIAZ MONTIEL\nESTUDIANTE INGENIERIA CIVIL" +
+            "\nUNIVERSIDAD DE SUCRE\n2016\nCEL: 3016922644\nCORREO: jucadimon@hotmail.com\nWEB: " +
+            "http://ingenieria-civil-y-pi.blogspot.com.co/ ";
+            Informacion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;//MiddleCenter/TopCenter
+            Informacion.Size = new Size(580, 380);//Tamaño del label
+            Informacion.Location = new Point(40, 35);//localizacion del label en el formulario
+            Informacion.Font = new Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
+            //  datos de la ventana
+            Subformulario.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            Subformulario.ClientSize = new System.Drawing.Size(640, 480);
+            Subformulario.Opacity = .85;
+            Subformulario.BackColor = Color.MintCream;//cambia color de fondo, Black, MintCream, MediumOrchid, 
+            Subformulario.ForeColor = Color.MediumOrchid;//cambia color de la fuente SlateBlue
+            Subformulario.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;//FixedDialog, None, Fixed3D
+            Subformulario.Name = "Subformulario";
+            Subformulario.Text = "SUB VENTANA X 1.0";
+            Subformulario.CancelButton = bsalirSub;
+            Subformulario.StartPosition = FormStartPosition.CenterScreen;
+            //
+            Subformulario.Controls.Add(bsalirSub);
+            Subformulario.Controls.Add(Titulo_SubFormulario);
+            Subformulario.Controls.Add(Informacion);
+            //
+            Subformulario.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //          EXPORTAR TXT 
+            ostipo = Environment.OSVersion.ToString();//saber el sistema operativo que se usa
+            SistemaOperativo = ostipo.Contains("Unix");
+            
+            archivoGenerado = new StringBuilder();//Definimos un comodin estatico
+            // string fecha = "";
+            // fecha = DateTime.Now.ToString();    //pedimos la hora al sistema y almacenamos en fecha
+            
+            archivoGenerado.AppendLine(txtCaja.Text);//añadimos la fecha al comodin
+
+			String reg = "";    //creamos un objeto tipo String del espacio de nombres Text
+            reg = archivoGenerado.ToString();    //convertimos el comodin a texto
+            
+            if (SistemaOperativo)
+            {
+                directorio1 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/texto.txt";
+                // string directorio2 = Environment.CurrentDirectory + "/texto"+fecha+".txt";
+            }
+            else
+            {
+                directorio1 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\texto.txt";
+                // string directorio2 = Environment.CurrentDirectory + "\\texto"+fecha+".txt";
+            }
+
+            archivoGeneradoA = new FileStream(directorio1, FileMode.Append);
+            archivoGeneradoA.Write(ASCIIEncoding.ASCII.GetBytes(reg), 0, reg.Length);//escribimos dentro del archivo
+            archivoGeneradoA.Close();//Cerra el archivo creado
+
+            MessageBox.Show("Exportación correcta! Revise el archivo en su escritorio.");
+        }
+    }
+}
